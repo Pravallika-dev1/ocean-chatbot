2cc6a5bfac80f114d6fedc0d26b2fc852aab716f
Pravallika-dev1 <pravallika3012@gmail.com>
2026-08-21 23:46:45 +0530

    Add CORS support

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 with open("knowledge.txt", "r", encoding="utf-8") as file:
     knowledge = file.read()
